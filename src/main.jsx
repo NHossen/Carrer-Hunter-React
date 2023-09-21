f6c08188827891d35d92b7067fdfcf58ae63ac97
Naeem Hossen <132334814+NHossen@users.noreply.github.com>
2023-09-22 02:38:15 +0400

    Basic Routing setup by react

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router=createBrowserRouter([
+  {
+    path:"/",
+    element: <section>
+      <div>Hello World</div>
+      <Outlet></Outlet>
+      
+    </section> ,
+
+    children:[
+      {
+        path:"/about",
+        element: <div>Chilred</div>
+      },
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
